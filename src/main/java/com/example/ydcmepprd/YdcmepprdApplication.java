53ad7695b0ae3527b35bf4b062fe6f98d4a5ff34
zhangjiasanshu <2563060341@qq.com>
2021-05-06 17:14:31 +0800

    zhangspringboot

---
+++ src/main/java/com/example/ydcmepprd/YdcmepprdApplication.java
@@ -0,0 +1,20 @@
+package com.example.ydcmepprd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableAsync
+@EnableCaching
+@EnableScheduling
+@SpringBootApplication
+
+public class YdcmepprdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YdcmepprdApplication.class, args);
+    }
+}
+
